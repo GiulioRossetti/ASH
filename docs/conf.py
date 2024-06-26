6df415a92f2ac4c056b88cf24bd716ce28d72baf
GiulioRossetti <giulio.rossetti@gmail.com>
2024-06-26 17:40:33 +0200

    :art: logo 2024

--- docs/conf.py
+++ docs/conf.py
@@ -12,18 +12,36 @@
 #
 import os
 import sys
-from mock import Mock as MagicMock
+
 import sphinx_rtd_theme
 
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+try:
+    from ash_model import __version__
+except ImportError:
+    __version__ = u'0.1.0'
 
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+version = __version__
+# The full version, including alpha/beta/rc tags.
+release = version
 
-MOCK_MODULES = [
-    "numpy",
+html_theme_options = {
+    "collapse_navigation": False,
+    "display_version": False,
+    "navigation_depth": 3,
+}
+
+# -- Project information -----------------------------------------------------
+
+project = "ASH"
+copyright = "2024, Giulio Rossetti"
+author = "Giulio Rossetti, Andrea Failla, Salvatore Citraro"
+
+autodoc_mock_imports = [
+     "numpy",
     "halp.undirected_hypergraph",
     "halp.utilities.undirected_matrices",
     "matplotlib.pyplot",
@@ -37,21 +55,7 @@ MOCK_MODULES = [
     "tqdm",
     "seaborn",
     "pandas",
-
 ]
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
-
-sys.path.insert(0, os.path.abspath('../'))
-
-# -- Project information -----------------------------------------------------
-
-project = 'ASH'
-copyright = '2023, Giulio Rossetti'
-author = 'Giulio Rossetti, Andrea Failla, Salvatore Citraro'
-
-# The full version, including alpha/beta/rc tags
-release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 
@@ -61,29 +65,34 @@ release = '0.1.0'
 extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.githubpages",
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
 ]
 
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
+# html_theme = 'alabaster'
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_logo = 'ash.png'
+
+# The name of an image file (relative to this directory) to use as a favicon of
+# the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+# html_favicon = None
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-#html_context = {"css_files": ["_static/css/custom.css"]}
