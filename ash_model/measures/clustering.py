af764a1f34cc4ae811b24b84475c73d23da6b04a
andreafailla <andrea.failla.ak@gmail.com>
2025-07-03 01:36:08 +0200

    ⚡️ improve performance

--- ash_model/measures/clustering.py
+++ ash_model/measures/clustering.py
@@ -1,102 +1,129 @@
 from math import comb
+from typing import Optional
 
-from ash_model.paths import *
+from ash_model.paths import ASH, is_s_path
 
 
 def s_local_clustering_coefficient(
-    h: ASH, s: int, hyperedge_id: str, start: int = None, end: int = None
+    h: ASH,
+    s: int,
+    hyperedge_id: str,
+    start: Optional[int] = None,
+    end: Optional[int] = None
 ) -> float:
     """
+    Compute the local clustering coefficient of a hyperedge within the s-overlap line graph of a hypergraph.
 
-    :param h:
-    :param s:
-    :param hyperedge_id:
-    :param start:
-    :param end:
-    :return:
+    The local clustering coefficient is defined as the ratio of the number of edges
+    actually present among the neighbors of the given node to the maximum possible
+    number of edges among those neighbors.
+
+    :param h:            an ASH instance
+    :param s:            minimum hyperedge overlap size for projection
+    :param hyperedge_id: identifier of the hyperedge in the line graph
+    :param start:        optional start time (inclusive)
+    :param end:          optional end time (inclusive)
+    :return:             local clustering coefficient in [0,1]
     """
-
+    # Build the s-overlap line graph once
     lg = h.s_line_graph(s, start, end)
-    if not lg.has_node(hyperedge_id):
-        return 1
+    if hyperedge_id not in lg:
+        return 0.0
 
-    ego = nx.ego_graph(lg, hyperedge_id)
-    ego.remove_node(hyperedge_id)
+    # Neighbors in the line graph correspond to overlapping hyperedges
+    neighbors = list(lg.neighbors(hyperedge_id))
+    k = len(neighbors)
+    if k < 2:
+        return 0.0
 
-    if ego.number_of_nodes() == 0:
-        return 0
+    # Induced subgraph on those neighbors
+    subgraph = lg.subgraph(neighbors)
+    actual_edges = subgraph.number_of_edges()
+    possible_edges = comb(k, 2)
 
-    triangle = 0
-    components = nx.connected_components(ego)
-    for c in components:
-        res = [hyperedge_id]
-        res.extend(c)
-        if len(res) > 2 and is_s_path(h, res):
-            triangle += ego.number_of_hyperedges()
-
-    denom = comb(2, ego.number_of_nodes())
-    if denom == 0:
-        return 0
-    LCC = triangle / denom
-    return LCC
+    return actual_edges / possible_edges
 
 
 def average_s_local_clustering_coefficient(
-    h: ASH, s: int, start: int = None, end: int = None
+    h: ASH,
+    s: int,
+    start: Optional[int] = None,
+    end: Optional[int] = None
 ) -> float:
     """
+    Compute the average local clustering coefficient across all hyperedges
+    in the s-overlap line graph.
 
-    :param h:
-    :param s:
-    :param start:
-    :param end:
-    :return:
+    :param h:     an ASH instance
+    :param s:     minimum hyperedge overlap size for projection
+    :param start: optional start time (inclusive)
+    :param end:   optional end time (inclusive)
+    :return:      average local clustering coefficient in [0,1], or 0 if no nodes
     """
+    lg = h.s_line_graph(s, start, end)
+    n = lg.number_of_nodes()
+    if n == 0:
+        return 0.0
 
-    LCCs = []
-    count = 0
-    for n in h.hyperedges(s, start, end):
-        count += 1
-        LCCs.append(s_local_clustering_coefficient(h, s, n, start, end))
-
-    if count > 0:
-        return sum(LCCs) / count
-
-    return 0
+    total = 0.0
+    for node in lg.nodes:
+        total += s_local_clustering_coefficient(h, s, node, start, end)
+    return total / n
 
 
-def s_intersections(h: ASH, s: int, start: int = None, end: int = None) -> float:
+def s_intersections(
+    h: ASH,
+    s: int,
+    start: Optional[int] = None,
+    end: Optional[int] = None
+) -> int:
     """
+    Count the number of s-overlap intersections (edges) in the hypergraph's
+    s-overlap line graph.
 
-    :param h:
-    :param s:
-    :param tid:
-    :return:
+    :param h:     an ASH instance
+    :param s:     minimum hyperedge overlap size
+    :param start: optional start time (inclusive)
+    :param end:   optional end time (inclusive)
+    :return:      number of intersections of size >= s
     """
-
-    g = h.s_line_graph(s, start, end)
-    return g.number_of_edges()
+    return h.s_line_graph(s, start, end).number_of_edges()
 
 
-def inclusiveness(h: ASH, start: int = None, end: int = None) -> float:
+def inclusiveness(
+    h: ASH,
+    start: Optional[int] = None,
+    end: Optional[int] = None
+) -> float:
     """
-    The inclusiveness function is a measure of the number of non-external hyperedges in an ASH.
-    It is defined as the ratio between the number of non-external hyperedges and
-    the hypergraph size. The higher this value, the more inclusive (or less exclusive)
-    the ASH is.
+    Computes the inclusiveness of the hypergraph over [start, end], defined as:
 
-    :param h: an ASH object
-    :param tid: temporal snapshot id
-    :return: inclusiveness value
+        (# of non-facet hyperedges) / (total # of hyperedges)
+
+    A *facet* hyperedge is one that is *not* a subset of any other hyperedge.
+    Non-facet hyperedges are those that *are* contained in at least one strictly larger hyperedge.
+
+    :param h:      an ASH instance
+    :param start:  optional start time (inclusive)
+    :param end:    optional end time (inclusive)
+    :return:       a float in [0,1], or 0.0 if there are no hyperedges
     """
+    node_sets = [set(he) for he in h.hyperedges(start, end, as_ids=False)]
+    total = len(node_sets)
+    if total == 0:
+        return 0.0
 
-    he_nodesets = [set(he) for he in h.hyperedges(start, end, as_ids=False)]
+    # Sort indices by descending set size to speed up subset checks
+    idxs = sorted(range(total), key=lambda i: len(node_sets[i]), reverse=True)
+    is_non_facet = [False] * total
 
-    non_facets = set()
-    for nset in he_nodesets:
-        for nset2 in he_nodesets:
-            if nset.issubset(nset2) and nset != nset2:
-                non_facets.add(he_nodesets.index(nset))
+    for i in range(total):
+        for j in idxs:
+            if i == j:
+                continue
+            if not is_non_facet[i] and node_sets[i].issubset(node_sets[j]):
+                is_non_facet[i] = True
+                break
 
-    # 1 - (toplexes/hyperedges)
-    return len(non_facets) / len(he_nodesets)
+    non_facet_count = sum(is_non_facet)
+    return non_facet_count / total
--- ash_model/test/test_clustering.py
+++ ash_model/test/test_clustering.py
@@ -1,55 +1,67 @@
 import unittest
-from ash_model import ASH
+
 from ash_model.measures import *
 
 
 class ClusteringTestCase(unittest.TestCase):
     def test_LCC(self):
-        a = ASH(hedge_removal=True)
+        a = ASH(edge_attributes=True)
         a.add_hyperedge([1, 2], 0)
         a.add_hyperedge([1, 3], 0)
         a.add_hyperedge([1, 4], 0)
 
         LCC = s_local_clustering_coefficient(a, 1, "e1")
-        self.assertEqual(LCC, 0)
+        self.assertEqual(LCC, 1)
 
-        a = ASH(hedge_removal=True)
+        a = ASH(edge_attributes=True)
         a.add_hyperedge([1, 2, 3, 5], 0)
         a.add_hyperedge([1, 2, 3, 4], 0)
         a.add_hyperedge([1, 2, 3, 4, 5], 0)
 
         LCC = s_local_clustering_coefficient(a, 1, "e1")
-        self.assertEqual(LCC, 0)
+        self.assertEqual(LCC, 1)
 
-        a = ASH(hedge_removal=True)
+        a = ASH(edge_attributes=True)
         a.add_hyperedge([1, 2, 6, 7], 0)
         a.add_hyperedge([1, 2, 3, 4, 5], 0)
         a.add_hyperedge([3, 4, 5, 6, 7], 0)
 
         LCC = s_local_clustering_coefficient(a, 1, "e1")
         self.assertEqual(LCC, 1)
+
+        a = ASH(edge_attributes=True)
+        a.add_hyperedge([1, 2], 0)   # “e1”
+        a.add_hyperedge([1, 3], 0)   # “e2”
+        a.add_hyperedge([1, 4], 0)   # “e3”
+        a.add_hyperedge([2, 5], 0)   # “e4”
+
+        lcc = s_local_clustering_coefficient(a, 1, "e1")
+        self.assertAlmostEqual(lcc, 1/3, places=6)
 
     def test_avg_LCC(self):
-        a = ASH(hedge_removal=True)
+        a = ASH(edge_attributes=True)
         a.add_hyperedge([1, 2], 0)
         a.add_hyperedge([1, 3], 0)
         a.add_hyperedge([1, 4], 0)
 
         LCC = average_s_local_clustering_coefficient(a, 1)
-        self.assertEqual(LCC, 0)
+        self.assertEqual(LCC, 1)
 
-        a = ASH(hedge_removal=True)
+        a = ASH(edge_attributes=True)
         a.add_hyperedge([1, 2, 3, 5], 0)
         a.add_hyperedge([1, 2, 3, 4], 0)
         a.add_hyperedge([1, 2, 3, 4, 5], 0)
 
         LCC = average_s_local_clustering_coefficient(a, 1)
-        self.assertEqual(LCC, 0)
+        self.assertEqual(LCC, 1)
 
-        a = ASH(hedge_removal=True)
+        a = ASH(edge_attributes=True)
         a.add_hyperedge([1, 2, 6, 7], 0)
         a.add_hyperedge([1, 2, 3, 4, 5], 0)
         a.add_hyperedge([3, 4, 5, 6, 7], 0)
 
         LCC = average_s_local_clustering_coefficient(a, 1)
-        self.assertEqual(LCC, 1)
+        self.assertEqual(LCC, 1) # TODO: CHECK THIS
+    
+
+    
