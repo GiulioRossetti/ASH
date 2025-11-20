21124454edbe16e65e17ad35018fce87a4131699
GiulioRossetti <giulio.rossetti@gmail.com>
2025-11-20 10:55:18 +0100

    :arrow_up: format metadata

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        "Development Status :: 4 - Beta ",
+        "Development Status :: 4 - Beta",
         # Indicate who your project is intended for
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Build Tools",
